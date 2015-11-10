e41b135fe93acf998649edd536e8140af8c7473c
Ryan Baumann <rsbaumann@gmail.com>
2015-11-09 21:33:23 -0600

    -updated activity limit to 400
    Signed-off-by: Ryan Baumann <rsbaumann@gmail.com>

--- queries/V_Stream_Linestring_Geojson.sql
+++ queries/V_Stream_Linestring_Geojson.sql
@@ -1,16 +1,9 @@
-﻿-- View: "V_Stream_LineString_Geojson"
-
--- DROP VIEW "V_Stream_LineString_Geojson";
-
-CREATE OR REPLACE VIEW "V_Stream_LineString_Geojson" AS 
- SELECT row_to_json(fc.*) AS row_to_json
+﻿ SELECT row_to_json(fc.*) AS row_to_json
    FROM ( SELECT 'FeatureCollection' AS type,
             array_to_json(array_agg(f.*)) AS features
            FROM ( SELECT 'Feature' AS type,
                     st_asgeojson(lg.linestring)::json AS geometry,
                     row_to_json(( SELECT l.*::record AS l
                            FROM ( SELECT lg.act_id) l)) AS properties
-                   FROM "V_Stream_LineString" lg) f) fc;
-
-ALTER TABLE "V_Stream_LineString_Geojson"
-  OWNER TO postgres;
+                   FROM "V_Stream_LineString" lg
+                   WHERE lg.ath_id=200623) f) fc;
