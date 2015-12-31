d328b21fa6740d0ffdc85f6365dd43ddddb91cc4
Ryan Baumann <rsbaumann@gmail.com>
2015-12-31 10:59:05 -0600

    Signed-off-by: Ryan Baumann <rsbaumann@gmail.com>

--- static/js/upload_img.js
+++ static/js/upload_img.js
@@ -15,7 +15,7 @@
 function get_signed_request(file, basepath_n){
     console.log(basepath_n+'sign_s3?file_name='+file.name+"&file_type="+file.type+'/');
     $.ajax({
-        url : basepath_n+'sign_s3?file_name='+file.name+"&file_type="+file.type,
+        url : basepath_n+'sign_s3?file_name='+file.name+"&file_type="+file.type+'/',
         type : "get",
         success : function(data) {
             var response = JSON.parse(data);
