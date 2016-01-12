dda2a12dfa342ea11342bbe5664f1b227951b379
Ryan Baumann <rsbaumann@gmail.com>
2016-01-11 20:44:29 -0600

    Signed-off-by: Ryan Baumann <rsbaumann@gmail.com>

--- app.py
+++ app.py
@@ -423,6 +423,31 @@ def testmap():
                            'heat_lines2/' + str(session['ath_id']),
                            ath_name=athlete.firstname + "_" + athlete.lastname + '_' + datetime.utcnow().strftime('%y%m%d'))
 
+@app.route('/demodesigner')
+def demodesigner():
+    """A demo map for those who want to try but dont have a Strava account"""
+    # First get the map extents so we can draw a point at the center
+    try:
+        avg_long, avg_lat = sp.get_acts_centroid(
+            engine, int(12904699))
+    except:
+        print "error retrieving map extents!"
+
+    #client = stravalib.client.Client(access_token=session['access_token'])
+    #athlete = client.get_athlete()
+
+    return render_template('strava_mapbox_gl_v3.html',
+                           avg_lat=avg_lat,
+                           avg_long=avg_long,
+                           mapbox_gl_accessToken=app.config[
+                               'MAPBOX_GL_ACCESS_TOKEN'],
+                           mapbox_accessToken=app.config[
+                               'MAPBOX_ACCESS_TOKEN'],
+                           heatpoint_url=BASEPATH +'heat_points/12904699',
+                           heatline_url=BASEPATH +'heat_lines/12904699',
+                           ath_name="ADV" + "_" + "Demo" + '_' + datetime.utcnow().strftime('%y%m%d'))
+
+
 
 @app.route('/delete_acts', methods=['POST'])
 def delete_acts():
